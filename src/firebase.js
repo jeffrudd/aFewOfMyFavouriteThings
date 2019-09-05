3aa7e613443a8b33370ec24ae2cb44c2f0a18401
Jeff Rudd <jeffrudd19@gmail.com>
2019-09-05 12:42:22 -0400

    Initializing Repo

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyANaDAteshM44MQ3R5vpuYWJdjcp6lxBvU",
+    authDomain: "project5-9e891.firebaseapp.com",
+    databaseURL: "https://project5-9e891.firebaseio.com",
+    projectId: "project5-9e891",
+    storageBucket: "",
+    messagingSenderId: "312412332690",
+    appId: "1:312412332690:web:876aae41c9596c26037c4b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
